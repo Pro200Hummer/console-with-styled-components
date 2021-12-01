840f1af7888c06221a6d41992e5546dace5a52e5
Pavel <nikolaj4ik.pasha@yandex.ru>
2021-12-01 20:21:17 +0300

    add wrapper component add global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import styled from "styled-components";
+
+const AppWrapper = styled.div`
+  width: 100%;
+  min-height: 100vh;
+  padding: 2rem;
+  background-color: #000000;
+`;
 
 export const App = () => {
-    return <div>
-
-    </div>
+    return <AppWrapper>
+        App
+    </AppWrapper>
 };
--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {App} from './App';
 import reportWebVitals from './reportWebVitals';
+import {createGlobalStyle} from "styled-components";
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+const Global = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: Consolas, serif;
+  }
+`;
+
+ReactDOM.render(
+    <>
+        <Global/>
+        <App/>
+    </>,
+    document.getElementById('root'));
 
 reportWebVitals();
